2ff402e24d9ce111ac60c1c789e0985270f657d8
rfdzhifa <rfdnazhifah@gmail.com>
2021-10-07 21:35:15 +0700

    first commit

---
+++ app/src/main/java/com/example/project_rencanain/new_task.kt
@@ -0,0 +1,11 @@
+package com.example.project_rencanain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_task)
+    }
+}
